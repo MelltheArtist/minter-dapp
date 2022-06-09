e0915d5ebe6e4cc0dfce74113468bdc921f6d37e
Mell the Artist <melltheartist48@gmail.com>
2022-06-09 07:15:19 -0400

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xdC176c8dfE12421D5047a4B374567023c3cD5aaf"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
